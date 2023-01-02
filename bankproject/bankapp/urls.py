5d782238a732969ba53ea19badedc548a57a27d6
johnjohnlog <106967217+johnjohnlog@users.noreply.github.com>
2023-01-02 23:47:35 +0530

    Update urls.py

--- bankproject/bankapp/urls.py
+++ bankproject/bankapp/urls.py
@@ -5,6 +5,7 @@ app_name='bankapp'
 urlpatterns = [
     path('',views.index,name='index'),
     path('enquiry/',views.enquiry,name='enquiry'),
+    path('detail/<int:id>',views.detail,name='detail'),
     path('create/',views.create,name='create'),
 
 ]
