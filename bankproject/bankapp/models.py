710b37f2d1f4eb75c7d5de1396404d2ec1d8bf3a
johnjohnlog <106967217+johnjohnlog@users.noreply.github.com>
2023-01-02 23:45:43 +0530

    Update models.py

--- bankproject/bankapp/models.py
+++ bankproject/bankapp/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+class Account(models.Model):
+    name = models.CharField(max_length=250)
+    age = models.IntegerField()
+    date = models.DateField()
+    address=models.TextField()
+    phone=models.IntegerField()
+    email=models.EmailField()
+    def __str__(self):
+        return self.name
